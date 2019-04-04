98cf258d6d75d5cadc1bc04f728f849e6a8ba19a
Nicholas Scheurich <nick@scheurich.me>
2019-04-04 02:55:59 -0500

    Increase text readability

--- src/screens/NotFound.tsx
+++ src/screens/NotFound.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { RouteComponentProps } from "@reach/router";
 
 const NotFound: React.FC<RouteComponentProps> = () => {
-  return <div>Nothing here!</div>;
+  return <div style={{ color: "#eee" }}>Nothing here!</div>;
 };
 
 export default NotFound;
